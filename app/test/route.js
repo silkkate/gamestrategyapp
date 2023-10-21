48b78ccd41908086f9509378ce604a4b8284c9c0
silkkate <silk.kate95@gmail.com>
2023-10-20 20:55:45 -0400

    route updated

--- app/test/route.js
+++ app/test/route.js
@@ -1,13 +1,23 @@
 export async function GET(request) {
-    // // const { searchParams } = new URL(request.url)
-    // const id = searchParams.get('id')
-    // const res = await fetch(`https://data.mongodb-api.com/product/${id}`, {
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //     'API-Key': process.env.DATA_API_KEY,
-    //   },
-    // })
-    // const product = await res.json()
-   console.log("Hello");
-    return Response.json({  })
-  }
+  const apiKey = process.env.GAME_API_KEY;
+  const openAiUrl = "https://api.openai.com/v1/engines/text-davinci-003/completions";
+
+  const gameTitle = request.body.gameTitle;
+  const aiPrompt = `Give me the best strategy to beat ${gameTitle}.`;
+
+  const response = await fetch(openAiUrl, {
+    method: "POST",
+    headers: {
+        'Authorization': `Bearer ${apiKey}`,
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        prompt: aiPrompt,
+        max_tokens: 1000,
+    }),
+  });
+
+  const data = await response.json();
+  response.status(200).json(data);
+   // return Response.json({  })
+  };
